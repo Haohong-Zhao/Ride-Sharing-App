1aed36d9ab37f4135451c4acfee4caf867329c94
hz147 <haohong.zhao@duke.edu>
2019-01-22 16:10:18 -0500

    create templates for index & about | import static files and setup static files structure

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('<h1>Hello Word</h1>')
+    return render(request, 'pages/index.html')
 
 def about(request):
-    return HttpResponse('<h1>About</h1>')
+    return render(request, 'pages/about.html')
