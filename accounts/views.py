9c6c856fdb74dd542e566aada2d71252d1e653cb
hz147 <haohong.zhao@duke.edu>
2019-01-24 10:44:14 -0500

    user register validation & account creation

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.models import User
 
 def login(request):
     if request.method == 'POST':
@@ -10,8 +11,32 @@ def login(request):
 
 def register(request):
     if request.method == 'POST':
-        messages.error(request, "Testing error message")
-        return redirect('register')
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        # validations
+        if password == password2:
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'Username is taken')
+                redirect('register')
+            
+            elif User.objects.filter(email=email).exists():
+                messages.error(request, 'Email is being used')
+                redirect('register')
+            
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password, first_name=first_name, last_name=last_name)
+                user.save()
+                messages.success(request, 'You are now registered and can login')
+                redirect('login')
+
+        else:
+            messages.error(request, 'Passwords do not match')
+            redirect('register')
     else:
         return render(request, 'accounts/register.html')
 
